8e2e6d6bd1674b4c90583bff3751b1b22139784e
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-08-14 20:25:47 +0530

    Added initial commit

---
+++ src/main/java/org/example/MethodExample.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MethodExample {
+    public static void sayHello() {
+        System.out.println("This is Hello from sayHello method");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        sayHello();
+    }
+
+}
