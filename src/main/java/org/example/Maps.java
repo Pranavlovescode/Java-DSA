8f57d987ca0b5df33faf6f7128c8ebd41be034ab
Pranavlovescode <pranavtitambe04@gmail.com>
2025-07-03 18:52:24 +0530

    frequency tracking of elements in map

--- src/main/java/org/example/Maps.java
+++ src/main/java/org/example/Maps.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Maps {
     public static void main(String[] args) {
-        HashMap<Integer,String> map = new HashMap<>();
+        Map<Integer,String> map = new HashMap<>();
         map.put(1,"Pranav");
         map.put(2,"Pramod");
         map.put(3,"Siddhant");
@@ -15,5 +18,28 @@ public class Maps {
         map.remove(5);
         System.out.println(map.values());
         System.out.println(map.containsKey(3));
+
+
+        Map<Character,Integer> freqMap = new HashMap<>();
+        String str = "abababhfdslfkdsfbabf";
+        for (char i:str.toCharArray()){
+            freqMap.put(i,freqMap.getOrDefault(i,0)+1); //used when we want to count the frequency of the elements in string, array,etc
+//            if(freqMap.containsKey(i)){
+//                freqMap.put(i,freqMap.get(i)+1);
+//            }
+//            else{
+//                freqMap.put(i,1);
+//            }
+        }
+        System.out.println(freqMap);
+
+        String string = "abababhfdslfkdsfbabf";
+        int window = 4;
+        List<String> result = new ArrayList<>();
+        for (int i=0;i<string.length()-window;i++){
+            String substr = string.substring(i,i+window);  //window size management->use only when window size is fixed
+            result.add(substr);
+        }
+        System.out.println(result);
     }
 }
