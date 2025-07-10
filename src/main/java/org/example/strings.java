05ae9bd595006c6d208c4df4a28fdabb43af0621
Pranavlovescode <pranavtitambe04@gmail.com>
2025-07-10 18:25:39 +0530

    strings in depth

---
+++ src/main/java/org/example/strings.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.Stack;
+
+public class strings {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack<>();
+        String s = "  hello world  ";
+        StringBuffer word = new StringBuffer();
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i)==' '){
+                if(!word.isEmpty()){
+                    st.push(word.toString());
+                    word.setLength(0);
+                }
+                i++;
+            }
+            else{
+                word.append(s.charAt(i));
+                i++;
+            }
+        }
+        if (!word.isEmpty()) {
+            st.push(word.toString());
+        }
+
+        System.out.println(st);
+        StringBuilder result = new StringBuilder();
+        while (!st.isEmpty()) {
+            result.append(st.pop());
+            if (!st.isEmpty()) result.append(" ");
+        }
+
+        System.out.println(result.toString());
+
+
+        int x = -121;
+        String string = Integer.toString(x);
+        StringBuilder value = new StringBuilder(string);
+        StringBuilder reverse = new StringBuilder(value.reverse());
+        value.reverse();
+        if(value.compareTo(reverse) == 0){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+        if (value.charAt(value.length() - 1) == '-') {
+            value.deleteCharAt(value.length() - 1);
+            // Remove leading zeros after reversing
+            while (value.length() > 1 && value.charAt(0) == '0') {
+                value.deleteCharAt(0);
+            }
+            value.insert(0, '-');
+        } else {
+            while (value.length() > 1 && value.charAt(0) == '0') {
+                value.deleteCharAt(0);
+            }
+        }
+
+        String finalStr = value.toString();
+        System.out.println(Integer.valueOf(finalStr));
+
+    }
+}
