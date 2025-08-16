3e1e9c138cc5e50116518cfeddd67f919584a334
Pranavlovescode <pranavtitambe04@gmail.com>
2025-08-16 17:39:27 +0530

    made some changes

--- src/main/java/org/example/Priority_Queue.java
+++ src/main/java/org/example/Priority_Queue.java
@@ -1,44 +1,47 @@
 package org.example;
 
 // Priority Queue as the name suggests that it gives the priority to the most highest value element in the queue according to the comparator function in the priority heap while creation of the queue
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 
 public class Priority_Queue {
-    public static void main(String[] args) {
-//        Min heap
-        PriorityQueue<Integer> pq = new  PriorityQueue(6);
+        public static void main(String[] args) {
+                // Min heap
+                PriorityQueue<Integer> pq = new PriorityQueue<>(6);
 
-//        for (int i = 0; i < 6; i++) {
-//            pq.add(i+9*7);
-//        }
-        pq.add(23);
-        pq.add(84);
-        pq.add(30);
-        pq.add(14);
-        pq.offer(5);
-        System.out.println(pq);
-        pq.poll();
-        System.out.println(pq);
-//        Checking the head of the Queue
-        System.out.println("The head is "+pq.peek());
-        Iterator iterator = pq.iterator();
-        System.out.println("Printing pq using iterator");
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
+                // for (int i = 0; i < 6; i++) {
+                // pq.add(i+9*7);
+                // }
+                pq.add(23);
+                pq.add(84);
+                pq.add(30);
+                pq.add(14);
+                pq.offer(5);
+                System.out.println(pq);
+                pq.poll();
+                System.out.println(pq);
+                // Checking the head of the Queue
+                System.out.println("The head is " + pq.peek());
+                Iterator<Integer> iterator = pq.iterator();
+                System.out.println("Printing pq using iterator");
+                while (iterator.hasNext()) {
+                        System.out.println(iterator.next());
+                }
+                System.out.println("The size is " + pq.size());
+
+                System.out.println(Arrays.toString(pq.toArray()));
+
+                // Max heap
+                PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+                pq2.add(23);
+                pq2.add(84);
+                pq2.add(30);
+                pq2.add(14);
+                pq2.add(5);
+                System.out.println(pq2);
+                int i = 6;
+                System.out.println(Integer.toString(i));
         }
-        System.out.println("The size is "+pq.size());
-
-        System.out.println(Arrays.toString(pq.toArray()));
-
-//        Max heap
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
-        pq2.add(23);
-        pq2.add(84);
-        pq2.add(30);
-        pq2.add(14);
-        pq2.add(5);
-        System.out.println(pq2);
-        int i = 6;
-        System.out.println(Integer.toString(i));
-    }
 }
--- src/main/java/org/example/Queue_Implementation.java
+++ src/main/java/org/example/Queue_Implementation.java
@@ -4,22 +4,22 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Queue_Implementation {
-    public static void main(String[] args) {
-        System.out.println("This is Queue Implementation using Linked List");
-        Queue<Integer> queue = new LinkedList<>();
-//        Adding an element in the queue
-        queue.offer(23);
-        queue.offer(234);
-        queue.offer(546);
-        queue.offer(2);
-        System.out.println(queue);
+        public static void main(String[] args) {
+                System.out.println("This is Queue Implementation using Linked List");
+                Queue<Integer> queue = new LinkedList<>();
+                // Adding an element in the queue
+                queue.offer(23);
+                queue.offer(234);
+                queue.offer(546);
+                queue.offer(2);
+                System.out.println(queue);
 
-//        Removing an element from the queue
-        queue.poll(); // This method can return the element even after the function is called
-        System.out.println(queue);
-        System.out.println(queue.poll());
+                // Removing an element from the queue
+                queue.poll(); // This method can return the element even after the function is called
+                System.out.println(queue);
+                System.out.println(queue.poll());
 
-//        Returning the first element in the queue
-        System.out.println("The first element in the queue is "+queue.peek());
-    }
+                // Returning the first element in the queue
+                System.out.println("The first element in the queue is " + queue.peek());
+        }
 }
